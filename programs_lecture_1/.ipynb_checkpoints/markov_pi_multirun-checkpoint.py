f501f00cc2994704a97a5d11664d9c347bd9d530
Catherine A Bernaciak <cabernaciak@cert.org>
2020-02-14 12:18:49 -0500

    some log file i think

---
+++ programs_lecture_1/.ipynb_checkpoints/markov_pi_multirun-checkpoint.py
@@ -0,0 +1,24 @@
+import random
+
+def markov_pi(N, delta): 
+    x, y = 1.0, 1.0
+    n_hits = 0
+    for i in range(N):
+        del_x, del_y = random.uniform(-delta, delta), random.uniform(-delta, delta)
+        if abs(x + del_x) < 1.0 and abs(y + del_y) < 1.0:
+            x, y = x + del_x, y + del_y
+        if x**2 + y**2 < 1.0: n_hits += 1
+    return n_hits
+
+# +
+pi_estimates = [] 
+n_runs = 1000
+n_trials = 4000
+delta = 0.1
+for run in range(n_runs):
+    pi_indiv_estimate = 4.0 * markov_pi(n_trials, delta) / float(n_trials)
+    pi_estimates.append(pi_indiv_estimate)
+    print(pi_indiv_estimate)
+    
+    
+    
